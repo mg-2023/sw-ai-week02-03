509fbd8752d761f97ac6dd509cb1578004470f17
김민기 <alsrl6710@gmail.com>
2026-09-04 17:45:09 +0900

    9월 4일

--- week3/1. basic/03_graph_basic.py
+++ week3/1. basic/03_graph_basic.py
@@ -41,13 +41,19 @@ def create_graph(vertices, edges, directed=False):
         그래프 딕셔너리
     """
     # TODO: 빈 그래프 초기화
-    pass
+    graph = {}
+    for vert in range(vertices):
+        graph[vert] = []
     
     # TODO: 간선 추가
     ## 간선 추가 (u에서 v로)
     ## 무방향 그래프면 반대 방향도 추가
-    pass
-    
+    for (u, v) in edges:
+        if v not in graph[u]:
+            graph[u].append(v)
+        if not directed:
+            if u not in graph[v]:
+                graph[v].append(u)
     return graph
 
 # 테스트 케이스
--- week3/1. basic/04_bfs.py
+++ week3/1. basic/04_bfs.py
@@ -47,13 +47,17 @@ def bfs(graph, start):
     
     # TODO: 큐 생성 및 시작 정점 추가
     ## 방문한 정점 집합
-    pass
+    queue = deque([start])
 
     # TODO: 큐가 빌 때까지 반복
     ## 큐에서 정점 꺼내기
     ## 인접한 정점들 확인
     ## 방문하지 않은 정점이면 큐에 추가
-    pass
+    while queue:
+        v = queue.popleft()
+        if v not in visited:
+            visited.append(v)
+            queue.extend(graph[v])
     
     return visited
 
